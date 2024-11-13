101b60cc884943ed29881eb8afcc75e3a8858dc1
617998 <sinchuri.617998@swl.k12.oh.us>
2024-11-13 09:59:24 -0500

    ini

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class InputHelper {
+    public static int getRangedInt(Scanner scan, String prompt, int min, int max) {
+        int num = 0;
+        boolean check = false;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                num = scan.nextInt();
+                if (num >= min && num <= max) {
+                    check = true;
+                } else {
+                    System.out.println("Input not within the range, please try again.");
+                }
+            } else {
+                System.out.println("Invalid input, please try again.");
+            }
+            scan.nextLine();
+        } while (!check);
+
+        return num;
+    }
 }
--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,2 +1,39 @@
-public class main {
+import java.util.Random;
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        int position = 0;
+        int valueAmount = 0;
+        int arrayPosition = 0;
+        int valuePostition;
+
+        //prints the random values assigned
+        int[] num = new int[100];
+        for (int i = 0; i < num.length; i++) {
+            position += 1;
+            num[i] = rand.nextInt(101);
+            System.out.println("Value " + position + " | " + num[i]);
+        }
+
+        int inputValue = InputHelper.getRangedInt(scan, "Enter a random number between 1 and 100:", 1, 100);
+        for (int i = 0; i < num.length; i++) {
+            if (inputValue == num[i]) {
+                arrayPosition = i;
+                valuePostition = i + 1;
+                valueAmount++;
+                if (valueAmount == 1) {
+                    System.out.println("The number " + inputValue + " was found at array position " + arrayPosition + " and value position " + valuePostition + ".");
+                }
+            }
+        }
+
+        if (arrayPosition == 0) {
+            System.out.println("Your value does not exist in this set of integers.");
+        } else {
+            System.out.println("The number " + inputValue + " was found " + valueAmount + " times.");
+        }
+    }
 }
